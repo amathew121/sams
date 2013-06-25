0001d574361f6a58c3c1883dd81a5935d50919fb
Ashish Mathew <amathew121@gmail.com>
2013-06-25 09:27:13 +0530

    Added user index page

---
+++ src/java/entities/FacultySubjectView.java
@@ -0,0 +1,125 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+/**
+ *
+ * @author Ashish
+ */
+public class FacultySubjectView {
+    
+    public int id_faculty_subject;
+    public String faculty_title;
+    public String facutly_lname;
+    public String facutly_fname;
+    public String subject_code;
+    public short subject_sr_no;
+    public String subject_name;
+    public String id_program;
+    public String id_course;
+    public short semester;
+    public String division;
+    public String batch;
+
+    public int getId_faculty_subject() {
+        return id_faculty_subject;
+    }
+
+    public void setId_faculty_subject(int id_faculty_subject) {
+        this.id_faculty_subject = id_faculty_subject;
+    }
+
+    public String getFaculty_title() {
+        return faculty_title;
+    }
+
+    public void setFaculty_title(String faculty_title) {
+        this.faculty_title = faculty_title;
+    }
+
+    public String getFacutly_lname() {
+        return facutly_lname;
+    }
+
+    public void setFacutly_lname(String facutly_lname) {
+        this.facutly_lname = facutly_lname;
+    }
+
+    public String getFacutly_fname() {
+        return facutly_fname;
+    }
+
+    public void setFacutly_fname(String facutly_fname) {
+        this.facutly_fname = facutly_fname;
+    }
+
+    public String getSubject_code() {
+        return subject_code;
+    }
+
+    public void setSubject_code(String subject_code) {
+        this.subject_code = subject_code;
+    }
+
+    public short getSubject_sr_no() {
+        return subject_sr_no;
+    }
+
+    public void setSubject_sr_no(short subject_sr_no) {
+        this.subject_sr_no = subject_sr_no;
+    }
+
+    public String getSubject_name() {
+        return subject_name;
+    }
+
+    public void setSubject_name(String subject_name) {
+        this.subject_name = subject_name;
+    }
+
+    public String getId_program() {
+        return id_program;
+    }
+
+    public void setId_program(String id_program) {
+        this.id_program = id_program;
+    }
+
+    public String getId_course() {
+        return id_course;
+    }
+
+    public void setId_course(String id_course) {
+        this.id_course = id_course;
+    }
+
+    public short getSemester() {
+        return semester;
+    }
+
+    public void setSemester(short semester) {
+        this.semester = semester;
+    }
+
+    public String getDivision() {
+        return division;
+    }
+
+    public void setDivision(String division) {
+        this.division = division;
+    }
+
+    public String getBatch() {
+        return batch;
+    }
+
+    public void setBatch(String batch) {
+        this.batch = batch;
+    }
+    
+    
+            
+            
+}
