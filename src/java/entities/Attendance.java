5bd7b0d50395365406236d097cc7d259ab54733a
Suraj Pawar <suraj.pawar30@gmail.com>
2014-03-24 17:44:50 +0530

    Fixed Documentation for Attendance.java

--- src/java/entities/Attendance.java
+++ src/java/entities/Attendance.java
@@ -21,7 +21,7 @@ import javax.persistence.Table;
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
- *
+ * POJO Entity for table 'attendance'
  * @author Ashish
  */
 @Entity
@@ -46,33 +46,64 @@ public class Attendance implements Serializable {
     @ManyToOne
     private CurrentStudent idCurrentStudent;
 
+    /**
+     * Creates Attendance Entity.
+     */
     public Attendance() {
     }
 
+    /**
+     * Creates Attendance Entity with the specified 'id_attendance'.
+     * @param idAttendance
+     */
     public Attendance(Long idAttendance) {
         this.idAttendance = idAttendance;
     }
 
+    /**
+     *
+     * @return
+     */
     public Long getIdAttendance() {
         return idAttendance;
     }
 
+    /**
+     *
+     * @param idAttendance
+     */
     public void setIdAttendance(Long idAttendance) {
         this.idAttendance = idAttendance;
     }
 
+    /**
+     *
+     * @return
+     */
     public Lecture getIdLecture() {
         return idLecture;
     }
 
+    /**
+     *
+     * @param idLecture
+     */
     public void setIdLecture(Lecture idLecture) {
         this.idLecture = idLecture;
     }
 
+    /**
+     *
+     * @return
+     */
     public CurrentStudent getIdCurrentStudent() {
         return idCurrentStudent;
     }
 
+    /**
+     *
+     * @param idCurrentStudent
+     */
     public void setIdCurrentStudent(CurrentStudent idCurrentStudent) {
         this.idCurrentStudent = idCurrentStudent;
     }
