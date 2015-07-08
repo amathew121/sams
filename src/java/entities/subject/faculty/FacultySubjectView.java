dfdf698f2fb44af7e1393c9c7ce773c9347f1b0e
Blessy Varghese <blessy15@gmail.com>
2015-07-08 11:06:57 +0530

    Sams dashboard updated for this academic semester..

--- src/java/entities/subject/faculty/FacultySubjectView.java
+++ src/java/entities/subject/faculty/FacultySubjectView.java
@@ -31,7 +31,7 @@ import javax.xml.bind.annotation.XmlRootElement;
     @NamedQuery(name = "FacultySubjectView.findAll", query = "SELECT f FROM FacultySubjectView f"),
     @NamedQuery(name = "FacultySubjectView.findByIdFacultySubject", query = "SELECT f FROM FacultySubjectView f WHERE f.idFacultySubject = :idFacultySubject"),
     @NamedQuery(name = "FacultySubjectView.findByIdFaculty", query = "SELECT f FROM FacultySubjectView f WHERE f.idFaculty = :idFaculty ORDER BY f.acDate DESC, f.idProgram, f.idCourse, f.semester, f.subjectCode, f.division, f.batch"),
-    @NamedQuery(name = "FacultySubjectView.findByIdFacultyCurrent", query = "SELECT f FROM FacultySubjectView f WHERE f.idFaculty = :idFaculty AND f.acDate = '2015-01-05' ORDER BY f.idProgram, f.idCourse, f.semester, f.subjectCode, f.division, f.batch"),
+    @NamedQuery(name = "FacultySubjectView.findByIdFacultyCurrent", query = "SELECT f FROM FacultySubjectView f WHERE f.idFaculty = :idFaculty AND f.acDate = '2015-07-08' ORDER BY f.idProgram, f.idCourse, f.semester, f.subjectCode, f.division, f.batch"),
     @NamedQuery(name = "FacultySubjectView.findByIdFacultyGroup", query = "SELECT f FROM FacultySubjectView f GROUP BY f.idFaculty"),
     @NamedQuery(name = "FacultySubjectView.findByFacultyTitle", query = "SELECT f FROM FacultySubjectView f WHERE f.facultyTitle = :facultyTitle"),
     @NamedQuery(name = "FacultySubjectView.findByFacultyLname", query = "SELECT f FROM FacultySubjectView f WHERE f.facultyLname = :facultyLname"),
